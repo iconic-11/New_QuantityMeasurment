0c16bb88435bf2680301fe2cea7ca859e4459225
iconic-11 <vyas.rss2303@gmail.com>
2026-01-30 17:29:29 +0530

    added new UC2 branch

--- src/main/java/org/example/QuantityMeasurmentApp.java
+++ src/main/java/org/example/QuantityMeasurmentApp.java
@@ -1,11 +1,8 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
 import java.util.Scanner;
 
-public class QuantityMeasurmentApp{
+public class QuantityMeasurmentApp {
 
     static class Feet {
         private final double value;
@@ -29,21 +26,76 @@ public class QuantityMeasurmentApp{
         }
     }
 
+
+    static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if (this == obj)
+                return true;
+
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+
+            Inches other = (Inches) obj;
+
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    static void CheckFeetEquality (double v1, double v2){
+        Feet f1 = new Feet(v1);
+        Feet f2 = new Feet(v2);
+
+        if(f1.equals(f2)){
+            System.out.println("Feet values are equal");
+        }
+        else{
+            System.out.println("Feet values are not equal");
+        }
+    }
+
+    static void CheckInchesEquality(double v1, double v2){
+        Inches i1 = new Inches(v1);
+        Inches i2 = new Inches(v2);
+
+        if(i1.equals(i2)){
+            System.out.println("Inches values are equal");
+        }
+        else{
+            System.out.println("Inches values are not equal");
+        }
+
+    }
+
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter first value in feet:");
-        double n1 = sc.nextDouble();
+        double ft1 = sc.nextDouble();
 
         System.out.println("Enter second value in feet:");
-        double n2 = sc.nextDouble();
+        double ft2 = sc.nextDouble();
 
-        Feet f1 = new Feet(n1);
-        Feet f2 = new Feet(n2);
 
-        boolean result = f1.equals(f2);
+        CheckFeetEquality(ft1, ft2);
 
-        System.out.println("Are both values equal? " + result);
+
+        System.out.println("Enter first value in Inches:");
+        double in1 = sc.nextDouble();
+
+        System.out.println("Enter second value in Inches:");
+        double in2 = sc.nextDouble();
+
+
+        CheckInchesEquality(in1, in2);
     }
 }
