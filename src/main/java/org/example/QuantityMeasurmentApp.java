cf05696f7c6f9a627dabca8d29821200d3e280df
iconic-11 <vyas.rss2303@gmail.com>
2026-02-02 11:38:15 +0530

    test case added in UC2

--- src/main/java/org/example/QuantityMeasurmentApp.java
+++ src/main/java/org/example/QuantityMeasurmentApp.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class QuantityMeasurmentApp {
 
-    static class Feet {
+    public static class Feet {
         private final double value;
 
         public Feet(double value) {
